a3d235b69739b1fce5085a612c08e51656c2ecad
Juan Manuel Fernandez <juanmanuelunlu@gmail.com>
2021-04-09 20:49:55 -0300

    Se actualizan los comandos de ayuda.

--- 2021/laboratorios/LAB02/resolucion/LAB02.R
+++ 2021/laboratorios/LAB02/resolucion/LAB02.R
@@ -1,80 +1,68 @@
-
 # Cargo dataframe
 MPI_national = read.csv('https://raw.githubusercontent.com/dm-uba/dm-uba.github.io/master/2021/laboratorios/LAB02/data/MPI_national.csv', header = TRUE, sep = ',')
 
 # Cargo dataframe
 MPI_subnational = read.csv('https://raw.githubusercontent.com/dm-uba/dm-uba.github.io/master/2021/laboratorios/LAB02/data/MPI_subnational.csv', header = TRUE, sep = ',')
 
+View(MPI_national)
+
 # Selecciono todas las combinaciones de ISO y world.region
-# A qué región pertenece cada país
 country.by.WR = unique(MPI_subnational[,c(1, 4)])
 
 # Verifico los nombres de los atributos de los dos df a integrar
 names(MPI_national)
 names(country.by.WR)
 
-# Hago el merge entre los dos df
 MPI_int = merge(MPI_national, country.by.WR, by.x = "ISO",  by.y = "ISO.country.code")
-
-
-# Verifico si ahora cuento con la columna World Region
 names(MPI_int)
+# MPI Urban: índice de pobreza multidimensional para áreas urbanas dentro del país
+# Headcount Ratio Urban: índice de recuento de la pobreza (% de la población que figura como pobre) dentro de las áreas urbanas del país
+# Intensity of Deprivation Urban: distancia promedio por debajo de la línea de pobreza de los que figuran como pobres en áreas urbanas.
+# MPI Rural: índice de pobreza multidimensional para áreas rurales dentro del país
+# Headcount Ratio Rural: índice de recuento de la pobreza (% de la población catalogada como pobre) dentro de las zonas rurales del país
+# Intensity of Deprivation Rural: distancia media por debajo de la línea de pobreza de los que figuran como pobres en las zonas rurales.
 
+# Indico la agregación en los nombres
+names(country.by.WR) = paste(names(country.by.WR), "_agg", sep="")
 
-#################################################
-###### Verifico atributos redundantes ##########
-
-
-# Calculo la matriz de correlación
 ds.cor = cor(MPI_int[,3:8])
-
-# Me quedo con solo una mitad
-ds.cor[upper.tri(ds.cor)] <- NA
-
-# Hago el heatmap agregando los parámetros symm = T y Rowv = F
+View(ds.cor)
 library(gplots)
 library(RColorBrewer)
-
 dev.off()
-par(mar=c(3,3,3,3))
-my_palette <- colorRampPalette(c("green", "black", "red"))(n = 100)
 heatmap.2(ds.cor,
           cellnote = round(ds.cor,2), 
-          notecol="white",     
+          notecol="black",     
           trace="none",        
-          margins = c(9,15), # 1er numero: margen inferior, 2do: margen izquierdo
-          col=my_palette,  
-          cexCol=0.75,
-          cexRow=0.75,
+          margins = c(9,22), # 1er numero: margen inferior, 2do: margen izquierdo
+          col=brewer.pal('RdYlBu', n=5),  
           dendrogram="none",
-          symm= T,
-          Rowv=F) 
-
+          cexCol=0.75,
+          cexRow=0.75)
 
 # Verificamos asociación
 plot(MPI_int[,3:8])
+
+# Boxplot
+boxplot(scale(MPI_int[,3:8]), cex.axis = 0.8, main = "Boxplot de los atributos numéricos")
 
 
 ###############################################
 ############### MANEJO DE RUIDO ###############
 
-# Boxplot
-par(mar=c(3,10,3,3))
-boxplot(scale(MPI_int[,3:8]),  cex.axis=0.75,main = "Boxplot de los atributos numéricos")
-
 # Plot de atributo 
-#plot(MPI_int$Intensity.of.Deprivation.Urban, main = "Plot de Intensity of deprivation Urban", xlab="índice del valor", ylab="valor")
-#plot(sort(MPI_int$Intensity.of.Deprivation.Urban), main = "Plot de Intensity of deprivation Urban", xlab="índice del valor", ylab="valor")
+plot(MPI_int$MPI.Urban, main = "Plot de Intensity of deprivation rural", xlab="índice del valor", ylab="valor")
+plot(sort(MPI_int$Intensity.of.Deprivation.Urban), main = "Plot de Intensity of deprivation rural", xlab="índice del valor", ylab="valor")
 
-# Calculamos el Coeficiente de Variación
 library(statip)
 apply(MPI_int[,3:8], 2, statip::cv, na_rm=TRUE)
 
 
-library(infotheo)
 BINS_EQ.F = 5
 BINS_EQ.W = 5
 atributo = MPI_int$MPI.Urban
+
+library(infotheo)
 
 # Discretize recibe el atributo, el método de binning y la cantidad de bins
 bin_eq_freq <- discretize(atributo,"equalfreq", BINS_EQ.F)
@@ -87,9 +75,9 @@ bin_eq_width <- discretize(atributo,"equalwidth", BINS_EQ.W)
 barplot(table(bin_eq_width), main = "Frecuencia de bins (eqWidth)")
 
 # ¿Qué problema tiene equalwidth?
-atributo2 = append(atributo, 100)
-bin_eq_width2 <- discretize(atributo2,"equalwidth", BINS_EQ.W)
-barplot(table(bin_eq_width2), main = "Frecuencia de bins (eqWidth)")
+# atributo2 = append(atributo, 100)
+# bin_eq_width <- discretize(atributo2,"equalwidth", BINS_EQ.W)
+# barplot(table(bin_eq_width), main = "Frecuencia de bins (eqWidth)")
 
 # Incorporo atributo al dataframe
 MPI_disc <- data.frame(atributo, bin_eq_freq, bin_eq_width)
@@ -101,7 +89,7 @@ names(MPI_disc) <- c('Atributo.Original', 'E.Freq.Bin', 'E.Width.Bin')
 MPI_disc[1:10,]
 
 # Por cada bin calculamos la media y reemplazamos en el atributo suavizado
-for(bin in 1:5){
+for(bin in 1:BINS_EQ.F){
   # Calculo de media para ambas técnicas de binnning
   media_bin_frq = mean(MPI_disc$Atributo.Original[MPI_disc$E.Freq.Bin==bin])
   media_bin_width = mean(MPI_disc$Atributo.Original[MPI_disc$E.Width.Bin==bin])
@@ -115,22 +103,10 @@ for(bin in 1:5){
 MPI_disc[1:10,]
 
 
-# grafico Sepal.Width ordenado de menor a mayor
-
 dev.off()
-plot(sort(MPI_disc$Atributo.Original) , type = "l", col="red", ylab = "Atributo", xlab = "Observaciones", main = "Dato original vs suavizado", cex=0.7)
 
-# Agrego la serie de la variable suavizada por anchos iguales
-lines(sort(MPI_disc$E.Width.Suav), type = "l", col="blue")
-# Agrego la serie de la variable suavizada por igual frecuencia
-lines(sort(MPI_disc$E.Freq.Suav),type = "l", col="green")
-
-
-legend("topleft", legend=c("Original", "Equal Width", "Equal Freq"), col=c("red", "blue", "green"), lty=1, cex = 0.7)
-
-# Mismo gráfico sin ordenar las instancias
-dev.off()
-plot(MPI_disc$Atributo.Original , type = "l", col="red", ylab = "Atributo", xlab = "Observaciones", main = "Dato original vs suavizado", cex=0.7)
+# grafico el atributo original ordenado de menor a mayor
+plot(MPI_disc$Atributo.Original, type = "l", col="red", ylab = "Atributo", xlab = "Observaciones", main = "Dato original vs suavizado", cex=0.7)
 
 # Agrego la serie de la variable suavizada por anchos iguales
 lines(MPI_disc$E.Width.Suav, type = "l", col="blue")
@@ -138,4 +114,4 @@ lines(MPI_disc$E.Width.Suav, type = "l", col="blue")
 lines(MPI_disc$E.Freq.Suav,type = "l", col="green")
 
 
-legend("topleft",legend=c("Original", "Equal Width", "Equal Freq"), col=c("red", "blue", "green"), lty=1, cex = 0.55)
+legend("topleft", legend=c("Original", "Equal Width", "Equal Freq"), col=c("red", "blue", "green"), lty=1, cex = 0.7)
